65f2a31cd45808676c62ef69e4e83049cdb4cc29
Chalaya22 <chalayaolga22@gmail.com>
2023-12-10 01:27:09 +0200

    comit 50

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,24 @@ import ContactForm from './ContactForm';
 import ContactList from './ContactList';
 import Filter from './Filter';
 
+const contactData = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    contacts: contactData,
+    filter: ' ',
   };
 
   componentDidMount() {
     const contactsInString = localStorage.getItem('contacts');
     const contactsIsParsed =
       JSON.parse(contactsInString) ?? this.state.contacts;
+
     this.setState({ contacts: contactsIsParsed });
   }
   componentDidUpdate(_, prevState) {
